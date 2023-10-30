0912ac99dd4460b3ac94ce1b71202a6ca1be3c9c
Cody Northrop <cnorthrop@google.com>
2023-10-30 08:36:43 -0400

    properties: EGL: Configure multi-file cache
    Start with 32MB cache size limit.
    
    Bug: 291316306
    Change-Id: Ib917c3199ac9a205d72ae6c32c0ee82b4b0aebe7
    Signed-off-by: rk134 <rahul@aospa.co>

--- config/common.mk
+++ config/common.mk
@@ -313,6 +313,11 @@ endif
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     arm64.memtag.process.system_server=off
 
+# EGL - Blobcache configuration
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.egl.blobcache.multifile=true \
+    ro.egl.blobcache.multifile_limit=33554432
+
 ifneq ($(wildcard vendor/google/modules/.),)
 # Flatten APEXs for performance
 OVERRIDE_TARGET_FLATTEN_APEX := true
