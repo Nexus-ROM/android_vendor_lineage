f382d5379261d1066a6e0237f9fe6818bd73bd91
minaripenguin <minaripenguin@users.noreply.github.com>
2024-08-17 07:58:33 +0800

    config: Disable memtag for different processes
    * it defaults to async if not specified
    
    Change-Id: I366d4875ac93a6b9e94354554aaf65fdd7d281f8
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- config/common.mk
+++ config/common.mk
@@ -314,9 +314,12 @@ PRODUCT_PRODUCT_PROPERTIES += \
     debug.sf.enable_transaction_tracing=false
 endif
 
-# Disable async MTE on system_server
+# Disable async MTE on a few processes
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    arm64.memtag.process.system_server=off
+    persist.arm64.memtag.app.com.android.se=off \
+    persist.arm64.memtag.app.com.google.android.bluetooth=off \
+    persist.arm64.memtag.app.com.android.nfc=off \
+    persist.arm64.memtag.process.system_server=off
 
 # EGL - Blobcache configuration
 PRODUCT_VENDOR_PROPERTIES += \
