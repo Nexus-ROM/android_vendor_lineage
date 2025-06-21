4e08beddde35285b915e5256616881b1c3d093a0
ralph950412 <ralph950412@gmail.com>
2025-06-21 12:56:06 +0800

    vendor: Import Google's Permission APEX
    Change-Id: I1a7359b95818c59bb7394d05a53e521f0dda22ce

--- config/common.mk
+++ config/common.mk
@@ -325,3 +325,9 @@ include vendor/lineage/config/version.mk
 # Enable Material Design 3 Expressive
 PRODUCT_PRODUCT_PROPERTIES += \
     is_expressive_design_enabled=true
+
+# APEX
+DISABLE_DEXPREOPT_CHECK := true
+
+PRODUCT_PACKAGES += \
+    com.google.android.permission
