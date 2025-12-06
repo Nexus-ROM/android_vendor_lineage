6b73f760c880e2fb2422210e271fe33b40b26523
CuriousNom <aryanbiswas7585@gmail.com>
2025-12-06 03:09:43 +0000

    config: Don't make gms inclusion conditional
    Change-Id: If8ac747a8bcb2bf24b579fb68fa8e369980e0794

--- config/common.mk
+++ config/common.mk
@@ -5,7 +5,7 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 -include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
 
 # GMS
-$(call inherit-product-if-exists, vendor/pixel/gms/products/gms.mk)
+$(call inherit-product, vendor/pixel/gms/products/gms.mk)
 $(call inherit-product, vendor/pixel/launcher/products/launcher.mk)
 $(call inherit-product, vendor/pixel/themepicker/products/themepicker.mk)
 $(call inherit-product, vendor/pixel/clocks/products/clocks.mk)
