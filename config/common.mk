58cb8ecedc321fc1a583cc0867ef2082b202b920
johnmart19 <johnivan19999@gmail.com>
2026-01-24 03:48:39 +0000

    config: Enable full ART optimizations with VDEX/ODEX
    Enable comprehensive ART optimizations including VDEX and ODEX
    generation for all system components to improve first-boot
    performance and reduce application startup latency.
    
    Configuration:
    - WITH_DEXPREOPT := true          # Global dexpreopt enable
    - DEX_PREOPT_DEFAULT := generate-vdex-and-image  # VDEX+ODEX gen
    - WITH_DEXPREOPT_DEBUG_INFO := false  # Exclude debug symbols
    
    Results:
    - Verified generation of .odex files (optimized dex)
    - Verified generation of .vdex files (verification dex)
    - ART images created for core system components
    - First boot time significantly reduced
    - Application startup latency improved
    
    Change-Id: I26d7e72e46ad3bec1ce70ef529f03d96ed9d1502

--- config/common.mk
+++ config/common.mk
@@ -139,6 +139,10 @@ ART_BUILD_TARGET_DEBUG := false
 ART_BUILD_HOST_NDEBUG := false
 ART_BUILD_HOST_DEBUG := false
 
+WITH_DEXPREOPT := true
+DEX_PREOPT_DEFAULT := generate-vdex-and-image
+WITH_DEXPREOPT_DEBUG_INFO := false
+
 # Disable dex2oat debug
 USE_DEX2OAT_DEBUG := false
 
